947c89569b0350267763f6c52961dd1b563e63ce
Sinisa Petkovic <sini.petkovic@gmail.com>
2018-03-30 23:28:53 -0400

    Shorten the load method

--- lib/cards.ex
+++ lib/cards.ex
@@ -40,12 +40,9 @@ defmodule Cards do
   end
 
   def load(filename) do
-    {status, binary} = File.read(filename)
-    
-    # Pattern matching case -> never use if
-    case status do
-      :ok -> :erlang.binary_to_term(binary)
-      :error -> "Could not read the file..."
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term(binary)
+      {:error, _reason} -> "Could not read the file..."
     end
 
   end
